f549a9038b67dd9bc0a44928f17214ef2f622e76
EMDOURADO <eliel.dourado27@gmail.com>
2016-03-22 21:14:07 -0300

    Versão 2.0!

--- MyFx/src/br/univel/TelaPrincipalController.java
+++ MyFx/src/br/univel/TelaPrincipalController.java
@@ -1,5 +1,23 @@
 package br.univel;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class TelaPrincipalController {
+
+    @FXML
+    private Button Botao;
+
+    @FXML
+    private TextField campo;
+
+    @FXML
+    void executar(ActionEvent event) {
+    	
+    	System.out.println(campo.getText());
+
+    }
 	
 }
